e47c92ad3ea9e2c003676aaed23d59dee3cc295c
k33g <ph.charriere@gmail.com>
2017-01-19 07:32:11 +0100

    one more thing

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 LV-426
 
 http://vertx.io/blog/my-first-vert-x-3-application/
+http://vertx.io/blog/some-rest-with-vert-x/
 
 ## Compile
 
--- src/main/java/org/k33g/acheron/WebApp.java
+++ src/main/java/org/k33g/acheron/WebApp.java
@@ -3,8 +3,13 @@ package org.k33g.acheron;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 
+
+import io.vertx.ext.web.Router;
+import io.vertx.core.http.HttpServerResponse;
+
 public class WebApp extends AbstractVerticle {
 
+  /*
   @Override
   public void start(Future<Void> fut) {
     vertx
@@ -20,4 +25,41 @@ public class WebApp extends AbstractVerticle {
           }
         });
   }
+  */
+
+  @Override
+  public void start(Future<Void> fut) {
+   // Create a router object.
+   Router router = Router.router(vertx);
+
+   // Bind "/" to our hello message - so we are still compatible.
+   router.route("/").handler(routingContext -> {
+     HttpServerResponse response = routingContext.response();
+     response
+         .putHeader("content-type", "text/html")
+         .end("<h1>Hello 🌍 from my first Vert.x 3 application</h1>");
+   });
+
+   // Create the HTTP server and pass the "accept" method to the request handler.
+   vertx
+       .createHttpServer()
+       .requestHandler(router::accept)
+       .listen(
+           // Retrieve the port from the configuration,
+           // default to 8080.
+           config().getInteger("http.port", 8080),
+           result -> {
+             if (result.succeeded()) {
+               fut.complete();
+             } else {
+               fut.fail(result.cause());
+             }
+           }
+       );
+}
+
+
+
+
+
 }
